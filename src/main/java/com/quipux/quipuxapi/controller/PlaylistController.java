1b35e8491c5f9f50b81b45f96567b2acad927d85
lucas da silva marinho <ls.marinho36@gmail.com>
2024-07-09 02:16:00 -0300

    chore: post and delete

--- src/main/java/com/quipux/quipuxapi/controller/PlaylistController.java
+++ src/main/java/com/quipux/quipuxapi/controller/PlaylistController.java
@@ -1,19 +1,24 @@
 package com.quipux.quipuxapi.controller;
 
 import java.util.List;
+import java.util.Optional;
 
+import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.quipux.quipuxapi.model.Playlist;
 import com.quipux.quipuxapi.repository.PlaylistRepository;
@@ -40,4 +45,15 @@ public class PlaylistController {
 	public ResponseEntity<Playlist> post(@Valid @RequestBody Playlist playlist){
 		return ResponseEntity.status(HttpStatus.CREATED).body(playlistRepository.save(playlist));
 	}
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	@DeleteMapping("/name/{name}")
+	@Transactional
+	public void deleteByName(@PathVariable String name) {
+	    Optional<Playlist> playlist = playlistRepository.findByName(name);
+	    if (playlist.isEmpty()) {
+	        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Playlist not found with name: " + name);
+	    }
+	    playlistRepository.deleteByName(name);
+	}
+	
 }
--- src/main/java/com/quipux/quipuxapi/repository/PlaylistRepository.java
+++ src/main/java/com/quipux/quipuxapi/repository/PlaylistRepository.java
@@ -1,6 +1,7 @@
 package com.quipux.quipuxapi.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
@@ -11,4 +12,8 @@ import com.quipux.quipuxapi.model.Playlist;
 @Repository
 public interface PlaylistRepository extends JpaRepository<Playlist, Long> {
 	public List<Playlist> findAllByNameContainingIgnoreCase(@Param("name") String name);
+
+	Optional<Playlist> findByName(String name);
+
+	void deleteByName(String name);
 }
