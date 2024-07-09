ba0f39e0f09e39fe01dfba980bfe4b384735e03a
lucas da silva marinho <ls.marinho36@gmail.com>
2024-07-09 01:17:38 -0300

    chore: create playlist controller

---
+++ src/main/java/com/quipux/quipuxapi/controller/PlaylistController.java
@@ -0,0 +1,28 @@
+package com.quipux.quipuxapi.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.quipux.quipuxapi.model.Playlist;
+import com.quipux.quipuxapi.repository.PlaylistRepository;
+
+@RestController
+@RequestMapping("/playlists")
+@CrossOrigin(origins = "*", allowedHeaders="*")
+public class PlaylistController {
+	
+	@Autowired
+	private PlaylistRepository playlistRepository;
+
+	
+	@GetMapping
+	public ResponseEntity<List<Playlist>> getAll(){
+		return ResponseEntity.ok(playlistRepository.findAll());
+	}
+}
--- src/main/java/com/quipux/quipuxapi/repository/PlaylistRepository.java
+++ src/main/java/com/quipux/quipuxapi/repository/PlaylistRepository.java
@@ -1,5 +1,11 @@
 package com.quipux.quipuxapi.repository;
 
-public interface PlaylistRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.quipux.quipuxapi.model.Playlist;
+
+@Repository
+public interface PlaylistRepository extends JpaRepository<Playlist, Long> {
 
 }
