45c98448b9e84fd9d7884012382a1593a0cd550e
lucas da silva marinho <ls.marinho36@gmail.com>
2024-07-09 00:40:18 -0300

    chore: playlist model

--- src/main/java/com/quipux/quipuxapi/model/Playlist.java
+++ src/main/java/com/quipux/quipuxapi/model/Playlist.java
@@ -1,5 +1,26 @@
 package com.quipux.quipuxapi.model;
 
-public class Playlist {
+import java.util.List;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="tb_quipux")
+public class Playlist {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String name;
+	private String description;
+	
+	
 }
