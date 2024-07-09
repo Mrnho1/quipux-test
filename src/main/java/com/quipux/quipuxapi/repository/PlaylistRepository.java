a7ed3385ba4f5988accce777323966360321e049
lucas da silva marinho <ls.marinho36@gmail.com>
2024-07-09 01:39:57 -0300

    feat: Get methods

--- src/main/java/com/quipux/quipuxapi/repository/PlaylistRepository.java
+++ src/main/java/com/quipux/quipuxapi/repository/PlaylistRepository.java
@@ -1,11 +1,14 @@
 package com.quipux.quipuxapi.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.quipux.quipuxapi.model.Playlist;
 
 @Repository
 public interface PlaylistRepository extends JpaRepository<Playlist, Long> {
-
+	public List<Playlist> findAllByNameContainingIgnoreCase(@Param("name") String name);
 }
